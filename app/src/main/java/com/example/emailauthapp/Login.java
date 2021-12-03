54642dfe5fac4371b7ee2ac1cf5e59581dcd82f0
sanele23 <sanele23@live.com>
2021-12-03 16:56:23 +0200

    Testing data storage

--- app/src/main/java/com/example/emailauthapp/Login.java
+++ app/src/main/java/com/example/emailauthapp/Login.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,75 +15,136 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Login extends AppCompatActivity {
+    EditText email,password;
+    Button loginBtn,gotoRegister;
+    boolean valid = true;
 
-    Button createAccountBtn, loginBtn;
-    EditText username, password; // to extract data
-    FirebaseAuth firebaseAuth;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        firebaseAuth = FirebaseAuth.getInstance(); // initialize
-//        Handle onClick event when create button is clicked
-        createAccountBtn = findViewById(R.id.createAccountBtn);
-        createAccountBtn.setOnClickListener(new View.OnClickListener() {
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        email = findViewById(R.id.loginEmail);
+        password = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginBtn);
+        gotoRegister = findViewById(R.id.gotoRegister);
+
+
+        // Login onclick listener
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkField(email);
+                checkField(password);
+
+                // According to access level, send user to the correct main activity
+                if (valid) {
+
+                    fAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            checkUserLevel(authResult.getUser().getUid());
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }
+            }
+        });
+
+
+        // Register onclick listener
+        gotoRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), Register.class));
             }
         });
 
-        username = findViewById(R.id.loginEmail);
-        password = findViewById(R.id.loginPassword);
-        loginBtn = findViewById(R.id.loginbtn);
+    }
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+    // check user access level
+    private void checkUserLevel(String uid) {
+        DocumentReference df = fStore.collection("Users").document(uid);
+        // extract data from the document
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
-            public void onClick(View v) {
-                // extract and validate data
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Log.d("TAG", "OnSuccess: " + documentSnapshot.getData());
+                // identify the user access level
+                if (documentSnapshot.getString("isTeacher") != null) {
+                    // user is admin
+                    startActivity(new Intent(getApplicationContext(), Admin.class));
+                    finish();
+                }
 
-                // if fields are empty
-                if (username.getText().toString().isEmpty()) {
-                    username.setError("Email is missing.");
-                    return;
+                if (documentSnapshot.getString("isStudent") != null) {
+                    // user is normal user(student)
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
                 }
-                if (password.getText().toString().isEmpty()) {
-                    password.setError("Password is missing.");
-                    return;
-                }
-                // data is valid
-                // login user
-                firebaseAuth.signInWithEmailAndPassword(username.getText().toString(),
-                        password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        // Login is success
-                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        finish();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(Login.this, e.getMessage(),Toast.LENGTH_SHORT).show();
-                    }
-                });
             }
         });
     }
 
-    // check firebase authentication
-    // if user had previously created account, then app will go straight to the menu
-    // the next time they open it
+    public boolean checkField(EditText textField){
+        if(textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
+
+        return valid;
+    }
+
+    // See if user has already signed in
     @Override
     protected void onStart() {
         super.onStart();
-        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            finish();
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            DocumentReference df = FirebaseFirestore.getInstance().collection("Users").document(
+                    FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+            df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    if (documentSnapshot.getString("isTeacher") != null) {
+                        startActivity(new Intent(getApplicationContext(), Admin.class));
+                        finish();
+                    }
+
+                    if (documentSnapshot.getString("isStudent") != null) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(getApplicationContext(), Login.class));
+                    finish();
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/emailauthapp/Register.java
+++ app/src/main/java/com/example/emailauthapp/Register.java
@@ -7,96 +7,162 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.badge.BadgeDrawable;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+    EditText fullName,email,password,phone;
+    Button registerBtn,goToLogin;
+    CheckBox isTeacherBox, isStudentBox;
 
-    EditText registerFullName, registerEmail, registerPassword,confirmPassword;
-    Button registerUserBtn,gotoLogin;
-
-    FirebaseAuth fAuth;
+    boolean valid = true;
+    FirebaseAuth fAuth; // firebase authentication
+    FirebaseFirestore fStore; // to store data on firestone
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        registerFullName = findViewById(R.id.registerFullName);
-        registerEmail = findViewById(R.id.registerEmail);
-        registerPassword = findViewById(R.id.registerPassword);
-        confirmPassword = findViewById(R.id.confirmPassword);
-
-        registerUserBtn = findViewById(R.id.registerBtn);
-        gotoLogin = findViewById(R.id.gotoLogin);
-
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
-        gotoLogin.setOnClickListener(new View.OnClickListener() {
+        fullName = findViewById(R.id.registerName);
+        email = findViewById(R.id.registerEmail);
+        password = findViewById(R.id.registerPassword);
+        phone = findViewById(R.id.registerPhone);
+        registerBtn = findViewById(R.id.registerBtn);
+        goToLogin = findViewById(R.id.gotoLogin);
+        isTeacherBox = findViewById(R.id.isTeacher);
+        isStudentBox = findViewById(R.id.isStudent);
+
+        // checkboxes logic
+        // user cannot check both checkboxes
+        isStudentBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                if (compoundButton.isChecked()) {
+                    isTeacherBox.setChecked(false);
+                }
+            }
+        });
+
+        isTeacherBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                if (compoundButton.isChecked()) {
+                    isStudentBox.setChecked(false);
+                }
+            }
+        });
+
+
+        // if data is valid, we can register the user
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkField(fullName);
+                checkField(email);
+                checkField(password);
+                checkField(phone);
+
+                // checkbox validation
+                if (!(isStudentBox.isChecked() || isTeacherBox.isChecked())) {
+                    Toast.makeText(Register.this, "Select the account type!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(valid) {
+                    // start the registration process
+                    fAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+
+                            FirebaseUser user = fAuth.getCurrentUser();
+
+                            // message is triggered when validation is a success
+                            Toast.makeText(Register.this, "Account Created", Toast.LENGTH_SHORT).show();
+
+                            // extract fullname, phone etc. data
+                            DocumentReference df = fStore.collection("Users").document(user.getUid());
+
+                            // store the data
+                            Map<String, Object> userInfo = new HashMap<>();
+                            userInfo.put("FullName", fullName.getText().toString());
+                            userInfo.put("UserEmail", email.getText().toString());
+                            userInfo.put("PhoneNumber", phone.getText().toString());
+
+                            // access level
+                            // specify if user is teacher or student
+                            if (isTeacherBox.isChecked()) {
+                                userInfo.put("isTeacher", "1");
+                            }
+
+                            if (isStudentBox.isChecked()) {
+                                userInfo.put("isStudent", "1");
+                            }
+
+
+                            df.set(userInfo);
+
+                            // When button is clicked, it should take user to the right activity
+                            if (isTeacherBox.isChecked()) {
+                                startActivity(new Intent(getApplicationContext(), Admin.class));
+                                finish();
+                            }
+
+                            if (isStudentBox.isChecked()) {
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                finish();
+                            }
+
+                        }
+                        // message is triggered if failed
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Register.this, "Failed to create an account", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+                }
+            }
+        });
+
+        goToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), Login.class));
-                finish();
             }
         });
 
-        registerUserBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // extract the data from the form
-                String fullName = registerFullName.getText().toString();
-                String email = registerEmail.getText().toString();
-                String password = registerPassword.getText().toString();
-                String confirmPass = confirmPassword.getText().toString();
+    }
 
-                // validate if text fields are empty
-                if(fullName.isEmpty()) {
-                    registerFullName.setError("Full name is required");
-                    return;
-                }
-                if(email.isEmpty()) {
-                    registerEmail.setError("Email is required");
-                    return;
-                }
-                if(password.isEmpty()) {
-                    registerPassword.setError("Password is required");
-                    return;
-                }
-                if(confirmPass.isEmpty()) {
-                    confirmPassword.setError("Confirm password is required");
-                    return;
-                }
+    public boolean checkField(EditText textField){
+        if(textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
 
-                // compare the two entered passwords
-                if(!password.equals(confirmPass)) {
-                    confirmPassword.setError("Passwords do not match");
-                    return;
-                }
-                // when the data is validated, register the user using firebase
-                Toast.makeText(Register.this, "Data Validated.", Toast.LENGTH_SHORT).show();
-
-                fAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        // send user to the home page
-                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                        // remove all the activity that was opened before main activity
-                        finish();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(Register.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
+        return valid;
     }
 }
